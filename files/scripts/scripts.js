bd62ec14dff1bf3ba0bb15e9223db6a2dee66d2d
Samuel Ajetunmobi <samuel.ajetunmobi@samual-ajetun.ca.sega.internal>
2016-04-21 13:21:49 +0100

    Added in games template and styling

---
+++ files/scripts/scripts.js
@@ -0,0 +1,15 @@
+/**
+ * Created by samuel.ajetunmobi on 21/04/16.
+ */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:3,
+        loop:true,
+        autoplay:true,
+        //autoplayTimeout:1000,
+        dots: true,
+        autoHeight: true,
+        autoWidth:true,
+        autoplayHoverPause:true
+    });
+});
